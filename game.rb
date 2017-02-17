6c3e6d1ed651bd65d142101d09076eb27da1f8f5
Gavin Stark <gavin@gstark.com>
2017-02-16 21:26:27 -0500

    Yay!

--- game.rb
+++ game.rb
@@ -2,6 +2,9 @@ require 'gosu'
 require_relative 'life'
 
 class Game < Gosu::Window
+  FRAMERATE = 1000 / 4.0
+  SCALE = 10
+
   def initialize
     super 640, 480
     self.caption = "Game of Lifes"
@@ -9,18 +12,47 @@ class Game < Gosu::Window
     @state = Set.new([
       [2,3], [3,3], [4,3]
     ])
+
+    @is_running = false
+    @space_down = false
+    @elapsed = 0
+  end
+
+  def screen_to_world(x, y)
+    return [(x / SCALE).floor, (y / SCALE).floor]
+  end
+
+  def world_to_screen(x, y)
+    return [x * SCALE, y * SCALE]
   end
 
   def update
-    @state = Life.tick(@state)
+    if Gosu.button_down? Gosu::KB_SPACE
+      @is_running = !@is_running if @space_down == false
+      @space_down = true
+    else
+      @space_down = false
+    end
+
+    if Gosu.button_down? Gosu::MS_LEFT
+      @state << screen_to_world(mouse_x, mouse_y)
+    end
+
+    if Gosu.milliseconds - @elapsed >= FRAMERATE
+      @state = Life.tick(@state) if @is_running
+      @elapsed = Gosu.milliseconds
+    end
   end
 
   def draw
-    Gosu.draw_rect(0, 0, 640, 480, Gosu::Color::WHITE)
+    bg = @is_running ? Gosu::Color::WHITE : Gosu::Color::GRAY
+    Gosu.draw_rect(0, 0, 640, 480, bg)
 
     @state.each do |x, y|
-      Gosu.draw_rect(x * 10, y * 10, 10, 10, Gosu::Color::BLACK)
+      Gosu.draw_rect(*world_to_screen(x, y), SCALE, SCALE, Gosu::Color::BLACK)
     end
+
+    Gosu.draw_rect(*world_to_screen(*screen_to_world(mouse_x, mouse_y)), SCALE, SCALE, Gosu::Color::RED)
   end
 end
 
